64f728756afce6862e30df3df644b75564e743ea
gevonte <52932127+gevonte@users.noreply.github.com>
2019-07-15 15:52:33 -0400

    Add files via upload

---
+++ gevonte code.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+ 
+
+5/8 #divison
+print("i am learing to code python today")
+print("123")
+print(5*2)
+print(5/2)
+print(5-2)
+print(5**2)
+print(8/9*2)
+print("the division 5/3 is", 5/2)
+#create function to sum any two numbers
+def plus(a,b,c):
+    plus = a + b + c
+    print(plus)
+
+plus(2,5,9)
+#print(plus)    
+
+
+def areatriangle (b,h):
+    areatriangle = (1/2)*b*h
+    print(areatriangle)
+    
+    
